8410544d0e366955a071ecb0e1d7f36b8d9269c5
Ilshat Zaitov <ilshat.zaitov.05@bk.ru>
2026-02-20 07:05:27 -0500

    Add warranty_period field to Product model (Lab8 Task4)

--- main/models.py
+++ main/models.py
@@ -23,6 +23,7 @@ class Product(models.Model):
     description = models.TextField()
     characteristics = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    warranty_period = models.IntegerField(default=12, help_text="Гарантийный срок в месяцах")
     stock_quantity = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, db_column="category_id")
     image = models.CharField(max_length=255, blank=True, null=True)
